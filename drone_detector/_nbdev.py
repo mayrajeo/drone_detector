19ca5a92a96d31827bb8629cf97f02a5b182be9c
Janne Mäyrä <janne.mayra@syke.fi>
2021-02-26 15:30:48 +0200

    fix .ini, add visualizations

--- drone_detector/_nbdev.py
+++ drone_detector/_nbdev.py
@@ -9,6 +9,7 @@ index = {"rangeof": "00_utils.ipynb",
          "make_grid": "01_tiling.ipynb",
          "Tiler": "01_tiling.ipynb",
          "untile_vector": "01_tiling.ipynb",
+         "untile_raster": "01_tiling.ipynb",
          "convert_poly_coords": "02_coordinates.ipynb",
          "affine_transform_gdf": "02_coordinates.ipynb",
          "gdf_to_px": "02_coordinates.ipynb",
@@ -18,16 +19,20 @@ index = {"rangeof": "00_utils.ipynb",
          "binary_mask_to_polygon": "03_coco.ipynb",
          "COCOProcessor": "03_coco.ipynb",
          "poly_IoU": "04_postprocessing.ipynb",
-         "poly_intersection_over_area": "04_postprocessing.ipynb"}
+         "poly_intersection_over_area": "04_postprocessing.ipynb",
+         "show_im_mask_pred": "05_visualizations.ipynb",
+         "plot_grid_preds_actuals_raws": "05_visualizations.ipynb",
+         "show_raw_mask_pred": "05_visualizations.ipynb"}
 
 modules = ["utils.py",
            "tiling.py",
            "coordinates.py",
            "coco.py",
-           "postproc.py"]
+           "postproc.py",
+           "visualizations.py"]
 
 doc_url = "https://jaeeolma.github.io/drone_detector/"
 
-git_url = "https://github.com/jaeeolma/drone_detector/tree/{branch}/"
+git_url = "https://github.com/jaeeolma/drone_detector/tree/master/"
 
 def custom_doc_links(name): return None

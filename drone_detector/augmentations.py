68da2ecc1db6ca0ea09ac6795fb946b56828c924
Janne Mäyrä <janne.mayra@syke.fi>
2021-03-29 12:28:49 +0300

    no it wasnt

--- drone_detector/augmentations.py
+++ drone_detector/augmentations.py
@@ -23,7 +23,7 @@ class AlbumentationsTransform(RandTransform):
         if self.idx == 0:
             aug_img = self.train_aug(image=np.array(img))['image']
         else:
-            if valid_aug is not None: aug_img = self.valid_aug(image=np.array(img))['image']
+            if self.valid_aug is not None: aug_img = self.valid_aug(image=np.array(img))['image']
             else: return img
         return PILImage.create(aug_img)
 
@@ -31,7 +31,7 @@ class AlbumentationsTransform(RandTransform):
         if self.idx == 0:
             aug_img = self.train_aug(image=np.array(img).transpose(1,2,0))['image'].transpose(2,0,1)
         else:
-            if valid_aug is not None: aug_img = self.valid_aug(image=np.array(img).transpose(1,2,0))['image'].transpose(2,0,1)
+            if self.valid_aug is not None: aug_img = self.valid_aug(image=np.array(img).transpose(1,2,0))['image'].transpose(2,0,1)
             else: return img
         return MultiChannelTensorImage.create(aug_img)
 

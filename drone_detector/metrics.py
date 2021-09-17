fix average_recall..kind of, update analyses

drone_detector/metrics.py
@@ -250,17 +250,19 @@ def average_precision(ground_truth:gpd.GeoDataFrame, preds:gpd.GeoDataFrame) -> 
 def average_recall(ground_truth:gpd.GeoDataFrame, preds:gpd.GeoDataFrame, max_detections:int=None) -> dict:
     """Get 11-point AR score for each label separately and with all iou_thresholds.
     If `max_detections` is not `None` evaluate with only that most confident predictions
+    Seems to be still bugged, needs fixing
     """
 
     # Clip geodataframes so that they cover the same area
     preds = gpd.clip(preds, box(*ground_truth.total_bounds), keep_geom_type=True)
     ground_truth = gpd.clip(ground_truth, box(*preds.total_bounds), keep_geom_type=True)
 
-    pred_sindex = preds.sindex
     tp_cols = [f'TP_{np.round(i, 2)}' for i in np.arange(0.5, 1.03, 0.05)]
     if max_detections is not None:
         preds.sort_values(by='score', ascending=False, inplace=True)
         preds = preds[:max_detections]
+        preds.reset_index(inplace=True)
+    pred_sindex = preds.sindex
     ground_truth[tp_cols] = ground_truth.apply(lambda row: is_true_positive(row, preds, pred_sindex),
                                                axis=1, result_type='expand')
     iou_threshs = np.arange(0.5, 1.04, 0.05)
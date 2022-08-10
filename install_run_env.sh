275404a4a14a4e8404c356187bb00d042c674415
Janne Mäyrä <janne.mayra@ymparisto.fi>
2022-08-10 10:02:47 +0000

    update install_*_env.sh -scripts, update readme

--- install_run_env.sh
+++ install_run_env.sh
@@ -1,17 +1,19 @@
 conda update conda -n base
-conda create --name dronedetector-dep python=3.9
+conda create --name dronedetector-deploy python=3.9
 conda activate dronedetector-deploy
 
 conda install mamba -c conda-forge
 
-pip install torch>=1.10.1 torchvision>=0.11.2 torchaudio>=0.10.1
-pip install fastai>=2.7.7
+mamba install -c fastchan fastai
 
-mamba install -c conda-forge geopandas>=0.11.0 rasterio>=1.3.0
-mamba install -c conda-forge jupyter_contrib_nbextensions
+mamba install -c conda-forge geopandas rasterio
 
 pip install opencv-python scikit-image
 
 pip install 'git+https://github.com/facebookresearch/detectron2.git'
 
 pip install albumentations
+pip install nbdev
+pip install -e .
+
+nbdev_test --skip_file_re examples
